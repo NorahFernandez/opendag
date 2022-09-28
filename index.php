fe8d4dae52255c5876a986bb0d0a5259e3750865
NorahFernandez <norahfernandezglansbeek@gmail.com>
2022-09-28 11:44:59 +0200

    Full site

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="img/logo/curio-03-geel-logo-rgb.png" alt="logo">
+        <p>gemaakt door: norah fernandez</p>
+    </header>
+    <main>
+        <div class="welcome">
+            <div class="welcomeText">
+                <h1>Welke uitdaging ga jij aan?</h1>
+                <h2>Welkom bij Curio, de beroepsopleider van de regio.</h2>
+            </div>
+        </div>
+
+        <div class="textAndPics">
+            <div class="text">
+                <h2>Vakken</h1>
+                <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique porro in voluptates quidem veniam enim neque adipisci dicta sit quisquam facilis futa mollitia, iure nostrum ad odio molestiae nisi ut?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum tempore esse dicta harum id explicabo provident optio dolores quam? Ea doloremque saepe in necessitatibus explicabo eaque et veniam, eligendi deserunt?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ipsam dicta assumenda impedit rerum natus nostrum quisquam accusamus eligendi error, eveniet harum, quam labore velit perferendis et enim similique?
+                </h3>
+            </div>
+            <img src="img/Mbo/Webinar afstandsleren.jpg" alt="foto">
+        </div>
+
+        <div class="overview">
+            <img src="img/Opleidingsoverzicht3.png" alt="overzicht">
+        </div>
+
+        <div class="textAndPics">
+            <img src="img/Mbo/oke-klas.jpg" alt="foto">
+            <div class="text">
+                <h2>Hoe zien de blokken eruit</h2>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eum beatae nihil iste, rem perferendis provident sunt doloremque ab eveniet nobis quasi illo, voluptates cupiditate deleniti maiores distinctio dolore magni!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias hic inventore vero dignissimos perferendis nobis tempora excepturi cum tenetur, reprehenderit delectus quae quaerat reiciendis illum in voluptas earum? Aliquam, quis.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam recusandae commodi, fuga laboriosam, impedit pariatur nisi cum dicta dolorum magnam reprehenderit ullam explicabo sequi itaque in dolores architecto quaerat iure.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore repellat ipsam? Quas aspernatur temporibus eaque obcaecati ducimus, libero aut laborum ipsam, expedita, sequi inventore optio ex magnam autem quibusdam.
+                </h3>
+            </div>
+        </div>
+
+        <div class="textAndPics">
+            <div class="yellow">
+                <h2>Mogelijkheid om te versnelling</h1>
+                <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique porro in voluptates quidem veniam enim neque adipisci dicta sit quisquam facilis futa mollitia, iure nostrum ad odio molestiae nisi ut?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum tempore esse dicta harum id explicabo provident optio dolores quam? Ea doloremque saepe in necessitatibus explicabo eaque et veniam, eligendi deserunt?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ipsam dicta assumenda impedit rerum natus nostrum quisquam accusamus eligendi error, eveniet harum, quam labore velit perferendis et enim similique?
+                </h3>
+            </div>
+            <img src="img/Mbo/talenten.jpg" alt="foto">
+        </div>
+
+        <div class="teacherBook">
+            <h2>smoelen boek</h2>
+            <div class="teachers">
+                <div class="teacher">
+                    <img src="img/teachers/dino.png" alt="docent foto">
+                    <h4>Elton Boekhoudt</h4>
+                </div>
+                <div class="teacher">
+                    <img src="img/teachers/lion.png" alt="docent foto">
+                    <h4>Ine Vermeer</h4>
+                </div>
+                <div class="teacher">
+                    <img src="img/teachers/penpen.png" alt="docent foto">
+                    <h4>Bart Roos</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="textAndPics">
+            <img src="img/Mbo/talenten.jpg" alt="foto">
+            <div class="text">
+                <h2>Hoe ziet het rooster eruit</h2>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eum beatae nihil iste, rem perferendis provident sunt doloremque ab eveniet nobis quasi illo, voluptates cupiditate deleniti maiores distinctio dolore magni!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias hic inventore vero dignissimos perferendis nobis tempora excepturi cum tenetur, reprehenderit delectus quae quaerat reiciendis illum in voluptas earum? Aliquam, quis.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam recusandae commodi, fuga laboriosam, impedit pariatur nisi cum dicta dolorum magnam reprehenderit ullam explicabo sequi itaque in dolores architecto quaerat iure.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore repellat ipsam? Quas aspernatur temporibus eaque obcaecati ducimus, libero aut laborum ipsam, expedita, sequi inventore optio ex magnam autem quibusdam.
+                </h3>
+            </div>
+        </div>
+
+        <div class="scedule">
+            <img src="img/scedule.png" alt="rooster">
+        </div>
+    </main>
+    <footer>
+        <p>&#169;Curio</p>
+    </footer>
+</body>
+</html>
